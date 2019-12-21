ef2237a3979aba553ff399e27114ef83e29fef0e
Willian Monoga <bytewareteam@gmail.com>
2019-12-21 11:10:22 -0500

    Añadir clientes al manejador de sesiones cuando se reciba el evento "authenticate"

--- User/listener.go
+++ User/listener.go
@@ -5,12 +5,13 @@ import (
 	"gitlab.com/test-ws/websocket"
 )
 
-func Listen(wsm *websocket.Manager)  {
+func Listen(wsm *websocket.Manager) {
 	go func(el *websocket.EventListener) {
 		channel := make(chan websocket.EventListenerData)
 		el.Subscribe("authenticate", channel)
-		for  {
+		for {
 			data := <-channel
+			wsm.ConnectionsManager.AddClient(data.Session.WsClient, data.Session)
 			l.Log("Data From Listener")
 			l.Debug(data.Session.Id, data.Event)
 		}
